6ecc16cf76e2174b85d07e10d21a2641a2ad2176
prashantnaik <prashantnaik@outlook.com>
2019-09-09 20:14:35 +0100

    Initial Commit

---
+++ src/main/java/com/serviceinfotech/springbootwithangular/SpringbootwithangularApplication.java
@@ -0,0 +1,13 @@
+package com.serviceinfotech.springbootwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithangularApplication.class, args);
+	}
+
+}
